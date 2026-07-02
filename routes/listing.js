87166648978974e7f777e31c9e12da2352d7b217
VENOM-18 <pravena.24.becs@acharya.ac.in>
2026-07-02 13:41:50 +0530

    implementation of session

--- routes/listing.js
+++ routes/listing.js
@@ -30,13 +30,22 @@ router.get("/new", (req, res) => {
 router.get("/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id).populate("reviews");
+    if (!listing) {
+        req.flash("error", "Listing not found");
+        return res.redirect("/listings");
+    }
     res.render("listings/show.ejs", { listing });
 }));
 
 //Create Route
 router.post("/", validateListing, wrapAsync(async (req, res, next) => {
+    if (!req.body.listing.image.url) {
+        req.body.listing.image.url =
+            "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=60";
+    }
     const newListing = new Listing(req.body.listing);
     await newListing.save();
+    req.flash("success", "New listing created successfully");
     res.redirect("/listings");
 }));
 
@@ -44,6 +53,10 @@ router.post("/", validateListing, wrapAsync(async (req, res, next) => {
 router.get("/:id/edit", wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
+    if (!listing) {
+        req.flash("error", "Listing not found");
+        return res.redirect("/listings");
+    }
     res.render("listings/edit.ejs", { listing });
 }));
 
@@ -51,6 +64,7 @@ router.get("/:id/edit", wrapAsync(async (req, res) => {
 router.put("/:id", validateListing, wrapAsync(async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    req.flash("success", "Listing updated");
     res.redirect(`/listings/${id}`);
 }));
 
@@ -59,6 +73,7 @@ router.delete("/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
+    req.flash("success", "Listing deleted");
     res.redirect("/listings");
 }));
 
