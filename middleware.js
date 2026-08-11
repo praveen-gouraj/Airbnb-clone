8986927a309057d562266e0db008627d5e6a6346
Praveen Gouraj <praveengouraj885@gmail.com>
2026-08-11 22:39:45 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -11,7 +11,6 @@ module.exports.isLoggedIn = (req, res, next) => {
     }
     next();
 }
-
 module.exports.saveRedirectUrl = (req, res, next) => {
     if (req.session.redirectUrl) {
         res.locals.redirectUrl = req.session.redirectUrl;
